6d18933de15c0d70427546e944521fe947a094aa
Anaid Fazlic <anaid.fazlic@edu.fit.ba>
2017-03-01 21:13:32 +0100

    added the body of webPlayer added html editor and output panel

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".toggleButton").hover(function () {
 
-    $(this).addClass("highlight");
+        $(this).addClass("highlight");
 
-}, function () {
+    }, function () {
 
-    $(this).removeClass("highlight");
+        $(this).removeClass("highlight");
 
-});
-$(".toggleButton").click(function () {
+    });
+    $(".toggleButton").click(function () {
 
-    $(this).toggleClass("active");
+        $(this).toggleClass("active");
 
-    $(this).removeClass("highlightedButton");
-});
+        $(this).removeClass("highlightedButton");
+    });
 
+    $(".panel").height($(window).height() - $('#header').height());
+    $('.panel').width($(window).width() / 2 - 5);
+    $('iframe').contents().find("html").html($('#htmlPanel').val());
+    $("textarea").on('change keyup paste', function () {
+        $('iframe').contents().find("html").html($('#htmlPanel').val());
+    });
 });
