af43d491bfda6f903ae6d4226bcc98d0f599c541
Anaid Fazlic <anaid.fazlic@edu.fit.ba>
2017-03-02 21:08:06 +0100

    css js panels added output updates when added new functionality in css and js panel

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    function updateOutput() {
+
+        $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+        document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+
+
+
+    }
     $(".toggleButton").hover(function () {
 
         $(this).addClass("highlight");
@@ -13,12 +22,16 @@ $(document).ready(function () {
         $(this).toggleClass("active");
 
         $(this).removeClass("highlightedButton");
+        var panelId = $(this).attr('id') + "Panel";
+        $("#" + panelId).toggleClass("hidden");
+        var numberOfActivePanels = 4 - $('.hidden').length
+        $('.panel').width($(window).width() / numberOfActivePanels - 6);
     });
 
     $(".panel").height($(window).height() - $('#header').height());
     $('.panel').width($(window).width() / 2 - 5);
     $('iframe').contents().find("html").html($('#htmlPanel').val());
     $("textarea").on('change keyup paste', function () {
-        $('iframe').contents().find("html").html($('#htmlPanel').val());
+         updateOutput();
     });
 });
